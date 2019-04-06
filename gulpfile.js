5608db3406cd272a0e5c9e320557ce5a2136ca0a
kensz66 <kensz66@126.com>
2019-04-06 22:04:01 +0800

    初始化

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync').create(),
+  reload = browserSync.reload;
+
+gulp.task('sass', function () {
+  gulp.src(['./scss/_bootstrap.scss'])
+    .pipe(
+      sass({
+        outputStyle: 'nested', //nested (default), compact, compressed, or expanded
+        cascade: true, //是否美化
+        remove: true,
+        //是否删除不必要的前缀
+      })
+        .on('error', sass.logError)
+    )
+    .pipe(concat('./style.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 20 versions'],
+      cascade: true,
+      remove: false
+    }))
+    .pipe(gulp.dest('./assets/css/'))
+    // .on('')
+    .pipe(reload({
+      stream: true
+    }));
+  console.log('Sass***文件发生改变' + new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds());
+});
+
+gulp.task('default', ['sass'], function () {
+  browserSync.init({
+
+    // proxy: "localhost:80"
+    server: {
+      baseDir: './',
+    }
+  });
+  gulp.watch(['./scss/*.scss', './scss/**/*.*'], ['sass']);
+  // gulp.watch(['./scss/test.scss'], ['sass1']);
+  gulp.watch(['./*.*', './templets/default/*.*', './include/*.*', './include/**/*.*', './assets/js/*.js', '!./node_modules/', '!./bower_components', '!./yys', '!./uploads', '!./install', '!./.git']).on('change', function (e) {
+    reload();
+    var d = new Date();
+    console.log(e.path + '更新成功:' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds());
+  });
+});
